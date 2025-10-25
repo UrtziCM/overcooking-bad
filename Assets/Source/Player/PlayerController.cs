e11161d666bb94d178f9d07425ddd40fdfba56df
Urtzi C. Mendizabal <urcrmedev@gmail.com>
2025-10-25 13:10:14 +0200

    update: FPS Controller

--- Assets/Source/Player/PlayerController.cs
+++ Assets/Source/Player/PlayerController.cs
@@ -1,36 +1,51 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController characterController;
-
-    private Vector2 inputVector;
-    private InputAction interactAction;
     [SerializeField]
-    private float SPEED = 10f;
+    private InputAction interactAction; 
+    [SerializeField]
+    private InputAction moveAction;
 
     private StateMachine stateMachine = new();
 
     private GameObject pickedItem;
+
+    [SerializeField]
+    float speed = 10.0f;
+
+    private Vector2 moveDirection => moveAction.ReadValue<Vector2>();
+
+    [SerializeField]
+    float sensitivity = 1f;
+
+    [SerializeField]
+    public GameObject cameraChild;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
-        
         stateMachine.ChangeState(new IdleState());
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        interactAction.Enable();
+        moveAction.Enable();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movementVector = new Vector3(inputVector.x, 0, inputVector.y);
-        characterController.Move(movementVector * SPEED * Time.deltaTime);
-    }
-    
-    public void Move(InputAction.CallbackContext context)
-    {
-        inputVector = context.ReadValue<Vector2>();
+        cameraChild.transform.eulerAngles += sensitivity * new Vector3(x: -Input.GetAxis("Mouse Y"), y: Mathf.Clamp(Input.GetAxis("Mouse X"), -90 , 90), z: 0);
+
+        Vector3 direction = cameraChild.transform.TransformDirection(moveDirection.x, 0, moveDirection.y);
+        direction.y = 0;
+        transform.position += speed * Time.deltaTime * (direction.normalized);
+
     }
 
     public void Interact(InputAction.CallbackContext context)
