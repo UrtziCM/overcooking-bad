fc3237a8f755b361d25a54c200bb38c14d76b82c
Gorka Urrutia del Pico <gorka_urrutiadelpico@euneiz.com>
2025-10-26 12:35:28 +0100

    update: ingredientera material now changes depending of the ingredient

--- Assets/Source/Counter/IngredienteraComponent.cs
+++ Assets/Source/Counter/IngredienteraComponent.cs
@@ -13,6 +13,13 @@ public class IngredienteraComponent : CounterComponent
     [SerializeField]
     private IngredientColor color = IngredientColor.Red;
 
+    [SerializeField]
+    private Material RedMaterial;
+    [SerializeField]
+    private Material GreenMaterial;
+    [SerializeField]
+    private Material BlueMaterial;
+
     private GameObject RedIngredient;
     private GameObject BlueIngredient;
     private GameObject GreenIngredient;
@@ -34,15 +41,46 @@ public class IngredienteraComponent : CounterComponent
             }
         }
     }
+    public Material IngredientMaterial
+    {
+        get
+        {
+            switch (color)
+            {
+                case IngredientColor.Red:
+                    return RedMaterial;
+                case IngredientColor.Green:
+                    return GreenMaterial;
+                case IngredientColor.Blue:
+                    return BlueMaterial;
+                default:
+                    return null;
+            }
+        }
+    }
+
+    private void ApplyMaterial()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        Material mat = IngredientMaterial;
+        renderer.sharedMaterial = mat;
+
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        ApplyMaterial();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void OnValidate()
+    {
+        ApplyMaterial();
     }
 }
