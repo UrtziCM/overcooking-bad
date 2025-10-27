cec9d16416bcd5fe4a4bb236fd5a68880e94524e
Urtzi C. Mendizabal <urcrmedev@gmail.com>
2025-10-27 14:51:49 +0100

    add: order class

---
+++ Assets/Source/Order/Order.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Order
+{
+    private const float MAX_SCORE_ON_CORRECT_ORDER = 2000;
+    PotionColour targetPotionColour;
+    public Order(PotionColour targetPotion)
+    {
+        this.targetPotionColour = targetPotion;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return targetPotionColour == ((obj as Order).targetPotionColour);
+    }
+
+    public bool IsPotionCorrect(PotionColour givenPotion)
+    {
+        return targetPotionColour == givenPotion;
+    }
+
+    public int OrderScore(PotionColour givenPotion, float humourScore = 1f)
+    {
+        if (IsPotionCorrect(targetPotionColour))
+        {
+            return (int)(MAX_SCORE_ON_CORRECT_ORDER * humourScore);
+        } else 
+            return 0;
+    }
+}
